566cff0d8733d4a9b1b5e9bacc1e4568e97750ad
Nikita Nyatin <yesnik@yandex.ru>
2020-09-13 13:00:46 +0500

    Create messenger.php

---
+++ messenger.php
@@ -0,0 +1,7 @@
+# Symfony Messenger
+
+[Messenger](https://symfony.com/doc/current/components/messenger.html) component helps applications send and receive messages to/from other applications or via message queues.
+
+```bash
+composer require symfony/messenger
+```
